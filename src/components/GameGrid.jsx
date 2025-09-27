b1db7c0c6147d21ea63ae0f5c68ecc92758e189e
reeloff7-lgtm <reeloff7@gmail.com>
2025-09-27 12:34:29 +0530

    Update GameGrid.jsx

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -2,10 +2,11 @@ import GameCard from "./GameCard";
 
 export default function GameGrid({ games, categoryKey }) {
   return (
-    <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+    <div className="grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
       {games.map((game) => (
         <GameCard key={game.id} game={game} categoryKey={categoryKey} />
       ))}
     </div>
   );
 }
+
